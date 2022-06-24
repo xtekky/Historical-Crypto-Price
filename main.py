122fbb10f9918980727bbce5ec02ac8ff946b90e
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-24 17:18:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import requests
 import json
 from colorama import Fore, Back, Style
 
-print(Fore.RED+Style.BRIGHT+'''
-      ___           ___           ___           ___           ___           ___     
-     |\__\         /\  \         /\  \         /\__\         /\__\         |\__\    
-     |:|  |        \:\  \       /::\  \       /:/  /        /:/  /         |:|  |   
-     |:|  |         \:\  \     /:/\:\  \     /:/__/        /:/__/          |:|  |   
-     |:|__|__       /::\  \   /::\~\:\  \   /::\__\____   /::\__\____      |:|__|__ 
- ____/::::\__\     /:/\:\__\ /:/\:\ \:\__\ /:/\:::::\__\ /:/\:::::\__\     /::::\__|
- \::::/~~/~       /:/  \/__/ \:\~\:\ \/__/ \/_|:|~~|~    \/_|:|~~|~       /:/~~/~   
-  ~~|:|~~|       /:/  /       \:\ \:\__\      |:|  |        |:|  |       /:/  /     
-    |:|  |       \/__/         \:\ \/__/      |:|  |        |:|  |       \/__/      
-    |:|  |                      \:\__\        |:|  |        |:|  |                  
-     \|__|   github.com/xtekky   \/__/         \|__|         \|__|                  
-''')
-
 coin = input(str(Fore.BLUE+'coin (bitcoin, ethereum etc...): '))
 date = input(Fore.BLUE+'date (dd-mm-yyyy): ')
 curr = input(str(Fore.BLUE+'currency (eur, usd etc...): '))
